b388de931481c36d7193f28ff544865bb80c855d
LanDev01 <alnjr0001@outlook.com>
2023-12-28 18:06:34 -0300

    studying inputs

---
+++ inputs.py
@@ -0,0 +1,10 @@
+# 1% das vendas
+vendas = float(input("Digite suas vendas do dia: "))
+
+bonus = vendas * 0.01
+print(bonus)
+
+num1 = float(input("Valor 1 "))
+num2 = float(input("Valor 2 "))
+
+print(f"Resultado {num1 + num2}")
