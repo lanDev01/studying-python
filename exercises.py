e55705d1d15e8eeb255086a38f03c7add8d829d2
LanDev01 <alnjr0001@outlook.com>
2024-01-02 12:17:32 -0300

    practicing with exercises

--- exercises.py
+++ exercises.py
@@ -23,3 +23,19 @@ print(f"Usuário {firstName} cadastrado com sucesso com o {email}\n")
 firstLetter = email[0]
 
 print(f"Enviamos um link de confirmação para o email {firstLetter}***{separator}\n")
+
+# Exercícios 2
+
+produtos = ["iphone", "ipad", "airpod", "monitor"]
+precos = [1500, 1000, 800, 2000]
+
+# Pegando o input já com letra minuscula
+product = input("Digite um produto ").lower()
+
+# Verificando se há o item na lista
+if(product in produtos):
+    #Pegando o index do valor dentro do array e passando como indice dos precos para retornar o valor
+    print(precos[produtos.index(product)])
+else:
+    # se não houver o produto ele retorna um print
+    print("Produto não cadastrado")
